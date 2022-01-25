5a8c589ff99ea68afbec28bb9aee1b4346f5bf50
EvcazinDeveloper <93777238+EvcazinDeveloper@users.noreply.github.com>
2022-01-25 15:29:32 -0300

    Updated PlayerArrow.js

--- js/Board.js
+++ js/Board.js
@@ -1,8 +1,10 @@
 class Board {
   constructor(x, y, width, height) {
+    
     var options = {
       isStatic: true
     };
+
 
     this.body = Bodies.rectangle(x, y, width, height, options);
 
@@ -15,12 +17,9 @@ class Board {
 
   display() {
     var pos = this.body.position;
-    var angle = this.body.angle;
     push();
-    translate(pos.x, pos.y);
-    rotate(angle);
     imageMode(CENTER);
-    image(this.image, 0, 0, this.width, this.height);
+    image(this.image, pos.x, pos.y, this.width, this.height);
     pop();
   }
 }
